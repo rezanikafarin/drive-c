58c2cd56e3a2dd9e0b963893e9691c01fc18003f
rezanikafarin <53050107+rezanikafarin@users.noreply.github.com>
2019-08-13 23:29:15 +0430

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var parent = document.querySelector('.box');
 var elem = document.createElement('div');
 var text =  document.createTextNode('Reza');
 elem.style.backgroundColor = 'green';
-elem.style.width = '300px';
+
 elem.style.padding = '10px';
 elem.appendChild(text);
 parent.appendChild(elem);
